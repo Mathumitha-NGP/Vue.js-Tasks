8fd46c475560b14e346063de2da4eeb0702b7e51
“Mathumitha-NGP” <mathumitha@ngpwebsmart.com>
2022-08-18 19:40:57 +0530

    arithmetic operation task

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,19 @@
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+      <h1 style="color:#42B983;">Arithmetc Operations</h1>
+      <p style="color:red;">Enter two integers:</p>
+      <label>a: </label>
+      <input type="number" v-model="input1"><br><br>
+      <label>b: </label>
+      <input type="number" v-model="input2"><br><br>
+      <button @click="add">Add </button><br><br> 
+      <button @click="sub">Subtract </button><br><br>
+      <button @click="mult">Multiply</button><br><br>
+      <button @click="div">Divide</button><br><br>
+      <input v-if="result>0" type="text" v-model="result" class="positive">
+      <input v-else type="text" v-model="result" class="negative">
+    </div>
 </template>
 
 <script>
@@ -36,10 +21,36 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+ data(){
+  return{
+    num1:'',
+    num2:'',
+    sum:'',
+    diff:'',
+    prod:'',
+    ratio:'',
+    result:''
   }
+ },
+ methods:{
+  add() {
+    this.result = (Number(this.input1)+Number(this.input2));
+
+  },
+  sub(){
+    this.result = (Number(this.input1)-Number(this.input2));
+
+  },
+  mult(){
+    this.result = (Number(this.input1) * Number(this.input2));
+  },
+  div(){
+    this.result = (Number(this.input1) / Number(this.input2));
+  }
+ },
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
@@ -54,6 +65,12 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #42B983;
+}
+.positive{
+  color: green
+}
+.negative{
+  color: red
 }
 </style>
